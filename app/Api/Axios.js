4b15135063f60366e2332ae539b482e6d39a1bb8
abdellah <abdellahedaoudi80@gmail.com>
2024-03-03 03:27:27 +0100

    add key Strapi and Url Using render.com

--- app/Api/Axios.js
+++ app/Api/Axios.js
@@ -2,13 +2,14 @@ const { default: axios } = require("axios");
 
 
 const Apikey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const ApiUrl = "http://localhost:1337/api";
+// const ApiUrl = "http://localhost:1337/api";
+const ApiUrl = "https://ecom-strapi-backend.onrender.com/api";
 
 const axiosClient = axios.create({
   baseURL: ApiUrl,
   headers: {
     Authorization: `Bearer ${Apikey}`
-  }  
+  }
 });
 
 export default axiosClient;
